04c4f07d2c56d3ce1a8327f1bb7d36427bba1d34
Install Jekyll <iam-mac2@IAM-MAC2s-MacBook-Pro.local>
2018-01-20 10:10:27 +0300

    Improve details and contacts.

---
+++ scripts/js/main.js
@@ -0,0 +1,18 @@
+/* Copyright (c) 2017 Andrea Buran [www.andreaburan.com]. All rights reserved */
+
+console.log('Liftoff!');
+
+var eMailbox = 'hello',
+    eDomain = 'andreaburan.com',
+    eAddress = (eMailbox + '@' + eDomain);
+
+console.log(eAddress);
+
+var eLink = document.createElement('a');
+    eLink.setAttribute('href', 'mailto:' + eAddress);
+    eLink.textContent = eAddress;
+
+console.log(eLink);
+
+var eEnvelope = document.getElementById('envelope');
+    eEnvelope.prepend(eLink);
