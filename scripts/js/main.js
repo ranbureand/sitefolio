c5fe27fbd48776da6ef73329ed5ebce07f3d19bf
Install Jekyll <iam-mac2@IAM-MAC2s-MacBook-Pro.local>
2018-01-23 00:03:27 +0300

    Replace Markdown footnote reverse link.

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -6,13 +6,16 @@ var eMailbox = 'hello',
     eDomain = 'andreaburan.com',
     eAddress = (eMailbox + '@' + eDomain);
 
-console.log(eAddress);
+//console.log(eAddress);
 
 var eLink = document.createElement('a');
     eLink.setAttribute('href', 'mailto:' + eAddress);
     eLink.textContent = eAddress;
 
-console.log(eLink);
+//console.log(eLink);
 
 var eEnvelope = document.getElementById('envelope');
     eEnvelope.prepend(eLink);
+
+var footnote = document.getElementsByClassName('reversefootnote');
+    footnote[0].textContent = 'Back to Number';
