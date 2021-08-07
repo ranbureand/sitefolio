1ca41c868734e46ec5b732a5144cf7792f1c7fd0
Andrea Buran <hello@andreaburan.com>
2021-08-07 08:01:18 +0300

    Improve minor things.

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -36,3 +36,31 @@ console.log(footnote);
 for (i = 0; i < footnote.length; i++) {
   footnote[i].textContent = 'Back to Number';
 }
+
+/*
+** Time
+*/
+
+/*function clock() {
+  var now = new Date(),
+      hours = now.getHours(),
+      minutes = now.getMinutes(),
+      ampm = hours >= 12 ? 'pm' : 'am';
+
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  var time = hours + ':' + minutes + ' ' + ampm;
+
+  document.getElementById('time').innerHTML = time;
+}
+
+clock();
+
+setInterval(clock, 10000);*/
+
+const now = new Date();
+const tz = now.getTimezoneOffset();
+
+console.log(tz);
