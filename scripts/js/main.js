dd2469e875ddf54010bcb7ba765eefa89c8f7453
Andrea Buran <hello@andreaburan.com>
2021-08-30 16:15:12 +0300

    Stickify the header.

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -31,7 +31,7 @@ for (i = 0; i < eEnvelope.length; i++) {
 
 var footnote = document.getElementsByClassName('reversefootnote');
 
-console.log(footnote);
+//console.log(footnote);
 
 for (i = 0; i < footnote.length; i++) {
   footnote[i].textContent = 'Go Back';
@@ -41,7 +41,8 @@ for (i = 0; i < footnote.length; i++) {
 ** Time
 */
 
-/*function clock() {
+/*
+function clock() {
   var now = new Date(),
       hours = now.getHours(),
       minutes = now.getMinutes(),
@@ -58,23 +59,66 @@ for (i = 0; i < footnote.length; i++) {
 
 clock();
 
-setInterval(clock, 10000);*/
+setInterval(clock, 10000);
 
 const now = new Date();
 const tz = now.getTimezoneOffset();
 
 console.log(tz);
-
-/*
-**
 */
 
-const article = document.querySelector(".article-body");
-const headings = article.querySelectorAll("h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]");
+/*
+** Anchored headings
+*/
 
-for (var i = 0; i < headings.length; i++) {
-  headings[i].innerHTML =
-    '<a href="#' + headings[i].id + '">' +
-        headings[i].innerText +
-    '</a>';
+// Declare variables
+var article,
+    headings;
+
+// Get the .article-body element
+article = document.querySelector(".article-body");
+
+// Initialize the headings variable
+if (article !== null) {
+  headings = article.querySelectorAll("h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]");
+
+  for (var i = 0; i < headings.length; i++) {
+    headings[i].innerHTML =
+      '<a href="#' + headings[i].id + '">' +
+          headings[i].innerText +
+      '</a>';
+
+    console.log('Anchored heading ' + headings[i].innerText + ' created.');
+  }
+} else {
+  headings = null;
+
+  console.log('No headings created.')
+}
+
+/*
+** Sticky header
+*/
+
+// When the window is scrolled, execute the function stickify()
+window.onscroll = function() { stickify() };
+
+// Get the .canvas-header and .header elements
+const canvasHeader = document.querySelector(".canvas-header"),
+      header = document.querySelector(".header");
+
+// Get the offset position of the .header element
+var   offset = header.offsetTop;
+
+// When the window is resized, get again the offset position of the .header element
+window.onresize = function(){ offset = header.offsetTop };
+
+// When you reach the position of the .header element, add the class .sticky to the .canvas-header element
+// When you unreach the position of the .header element, remove the class .sticky from the .canvas-header element
+function stickify() {
+  if ( window.pageYOffset > offset ) {
+    canvasHeader.classList.add('sticky');
+  } else {
+    canvasHeader.classList.remove('sticky');
+  }
 }
